b8301882f87827491e401e4b23b46a4f2820935f
Vasilis Ethan Sarris <vas82@pitt.edu>
2020-11-17 02:02:11 -0500

    Update bot.py

--- test/bot.py
+++ test/bot.py
@@ -161,7 +161,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1457818035:AAFHuUULruI0ULhuhbNevSJEDBbXABZVfw8", use_context=True)
+    updater = Updater("TOKEN", use_context=True)
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('help', help_command))
